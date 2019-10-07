3786a1f4a3d14d837ef6e6df9c17c3ac6c4b028d
Jason Jewel <jjewe2009@gmail.com>
2019-10-07 14:08:28 +1000

    Fix square select to not allow overriding values

--- src/index.js
+++ src/index.js
@@ -30,6 +30,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ 
       squares: squares,
@@ -60,7 +63,7 @@ class Board extends React.Component {
   render() {
     const winner = this.calculateWinner(this.state.squares);
     let status;
-    
+
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
